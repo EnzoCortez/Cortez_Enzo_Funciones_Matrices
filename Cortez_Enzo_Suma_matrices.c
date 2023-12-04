cec54200c1d26de3a5665d51504d353d0982bde5
Enzo Cortez <enzo.cortez@udla.edu.ec>
2023-12-03 19:41:58 -0500

    Generacion de matrices

--- Cortez_Enzo_Suma_matrices.c
+++ Cortez_Enzo_Suma_matrices.c
@@ -8,3 +8,13 @@ void solicitarDimensiones(int *m, int *n) {
     scanf("%d", m);
     printf("Ingrese el número de columnas: ");
     scanf("%d", n);
+}
+
+void generarMatriz(int m, int n, int matriz[m][n]) {
+    srand(time(0));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matriz[i][j] = rand() % 100 + 1;
+        }
+    }
+}
